cfe85cc1fad733a1db1afa88e624022c496a45b1
Oleksii Rudenko <oleksiirudenkomail@gmail.com>
2019-06-19 19:51:32 +0300

    added to aws

--- Camagru/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	define('DSN', 'mysql:host=localhost;charset=utf8mb4');
-	define('USERNAME', 'root');
-	//define('PASSWORD', 'root'); // for home
-	define('PASSWORD', 'qweqwe'); // for unit
-	define('DBNAME', 'camagru');
